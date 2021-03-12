d9c52dc5a89f05cbacd839e9b51dcc52301a9980
kanzff <rivakanzf@gmail.com>
2021-03-12 23:51:16 +0700

    newsletter

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 let notif = document.getElementById('notif')
+let newsletter = document.getElementById('newsletter')
 
 function slideNotif() {
   notif.classList.toggle('hidden')
+}
+
+function hideNewsletter() {
+  newsletter.classList.toggle('hidden')
 }
